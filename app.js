9e18fb77a6fa38e4e08f71bef4f2773500823a5f
Ngo Huu Tien <tiennh@innotech-vn.com>
2020-01-15 22:15:51 +0700

    rf

--- app.js
+++ app.js
@@ -51,6 +51,13 @@ app.use(function(req, res, next) {
     next(err);
 });
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
 // error handlers
 
 // development error handler
