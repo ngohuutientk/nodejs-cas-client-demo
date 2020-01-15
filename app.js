213926b77b7fde1f1c5f44c9264abeb3eaf6e46f
Ngo Huu Tien <tiennh@innotech-vn.com>
2020-01-15 22:20:38 +0700

    up

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ app.use(function(req, res, next) {
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
+    var err = new Error('Not Found');
     next(err);
 });
 
