eb490b4aa5eedf9eee928be91740de36520b46e5
ngohuutientk <42372998+ngohuutientk@users.noreply.github.com>
2020-01-15 22:14:17 +0700

    Update app.js
    rf

--- app.js
+++ app.js
@@ -44,12 +44,6 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
 
 // error handlers
 
